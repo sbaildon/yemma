197ef9f840a501e8a610fbe2a8f148afef1f4645
Sean Baildon <sean@baildon.co>
2022-01-19 19:54:39 +0000

    blah

--- config/test.exs
+++ config/test.exs
@@ -1,16 +1,14 @@
 import Config
 
-config :yemma,
-  ecto_repos: [Yemma.Test.Repo]
-
-config :yemma, Yemma.Test.Repo,
+config :yemma, YemmaTest.Repo,
   priv: "test/support/",
   database: Path.expand("../yemma_test.db", Path.dirname(__ENV__.file)),
   pool_size: 5,
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :yemma,
-  repo: Yemma.Test.Repo,
+  repo: YemmaTest.Repo,
+  ecto_repos: [YemmaTest.Repo],
   pubsub_server: Phoenix.YemmaTest.PubSub,
   secret_key_base: "KGZHtZ5nYiaNleW9fCWoCjnAfRHY7gTl7S2+pzLKIN0paXk0Syv3826nJqdR/uiD"
 
