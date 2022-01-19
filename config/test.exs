b1c0915ecca8ee1461499852c4b55f23a0628b84
Sean Baildon <sean@baildon.co>
2022-01-19 17:22:49 +0000

    migrations for test

--- config/test.exs
+++ config/test.exs
@@ -6,6 +6,7 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :yemma, Yemma.Test.Repo,
+  priv: "test/support/",
   database: Path.expand("../yemma_test.db", Path.dirname(__ENV__.file)),
   pool_size: 5,
   pool: Ecto.Adapters.SQL.Sandbox
---
+++ test/support/migrations/20220119171316_add_users_table.exs
@@ -0,0 +1,7 @@
+defmodule Yemma.Test.Repo.Migrations.AddUsersTable do
+  use Ecto.Migration
+
+  def change do
+    Yemma.Migrations.change()
+  end
+end
