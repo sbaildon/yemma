db88dbda3ee428125ecf8aa3d09ae143a74ce9a7
Sean Baildon <sean@baildon.co>
2022-01-20 10:18:51 +0000

    no live_view, live_component, or router

--- lib/yemma_web.ex
+++ lib/yemma_web.ex
@@ -44,38 +44,11 @@ defmodule YemmaWeb do
     end
   end
 
-  def live_view do
-    quote do
-      use Phoenix.LiveView,
-        layout: {YemmaWeb.LayoutView, "live.html"}
-
-      unquote(view_helpers())
-    end
-  end
-
-  def live_component do
-    quote do
-      use Phoenix.LiveComponent
-
-      unquote(view_helpers())
-    end
-  end
-
   def component do
     quote do
       use Phoenix.Component
 
       unquote(view_helpers())
-    end
-  end
-
-  def router do
-    quote do
-      use Phoenix.Router
-
-      import Plug.Conn
-      import Phoenix.Controller
-      import Phoenix.LiveView.Router
     end
   end
 
