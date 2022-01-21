763d44de23802221b2b39abfc17e81b02345f77a
Sean Baildon <sean@baildon.co>
2022-01-21 23:02:44 +0000

    no :repo in user.ex

--- lib/yemma/users/user.ex
+++ lib/yemma/users/user.ex
@@ -2,8 +2,6 @@ defmodule Yemma.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-  defp repo(), do: Application.fetch_env!(:yemma, :repo)
-
   schema "users" do
     field :email, :string
     field :confirmed_at, :naive_datetime
@@ -30,7 +28,6 @@ defmodule Yemma.Users.User do
     |> validate_required([:email])
     |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
     |> validate_length(:email, max: 160)
-    |> unsafe_validate_unique(:email, repo())
     |> unique_constraint(:email)
   end
 
