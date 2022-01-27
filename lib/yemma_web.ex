2948868c2a9b2af900c33dc4ca9baa1e5219bf9f
Sean Baildon <sean@baildon.co>
2022-01-27 21:58:33 +0000

    remove yemmaweb comments

--- lib/yemma_web.ex
+++ lib/yemma_web.ex
@@ -1,22 +1,4 @@
 defmodule YemmaWeb do
-  @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, views, channels and so on.
-
-  This can be used in your application as:
-
-      use YemmaWeb, :controller
-      use YemmaWeb, :view
-
-  The definitions below will be executed for every view,
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below. Instead, define any helper function in modules
-  and import those modules here.
-  """
-
   def controller do
     quote do
       use Phoenix.Controller, namespace: YemmaWeb
@@ -47,23 +29,19 @@ defmodule YemmaWeb do
         root: "lib/yemma_web/templates",
         namespace: YemmaWeb
 
-      # Import convenience functions from controllers
       import Phoenix.Controller,
         only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
 
-      # Include shared imports and aliases for views
       unquote(view_helpers())
     end
   end
 
   defp view_helpers do
     quote do
-      # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
 
       import Phoenix.LiveView.Helpers, only: [form: 1]
 
-      # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
 
       import YemmaWeb.ErrorHelpers
